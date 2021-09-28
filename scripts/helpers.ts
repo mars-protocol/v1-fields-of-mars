9f2167e853e7803f12a9911cb9bed20b5597f719
The Larry <26318510+0xlarry@users.noreply.github.com>
2021-09-28 17:54:30 +0100

    Remove logics related to `astroport_pair_stable` (unnecessary for testing)

--- scripts/helpers.ts
+++ scripts/helpers.ts
@@ -13,41 +13,21 @@ import {
   LCDClient,
 } from "@terra-money/terra.js";
 
-// Related to transactions
-export const GAS_LIMIT = 30000000;
-export const GAS_AMOUNT = 4500000;
-
-// Related to calculation of tax
-export const DECIMAL_FRACTION = new BN("1000000000000000000");
-
-// Related to calculation of commissions
-export const THREE = new BN("3");
-export const THOUSAND = new BN("1000");
-
-// Related to calculation of StableSwap output
-export const ITERATIONS = 32;
-export const ONE = new BN("1");
-export const TWO = new BN("2");
-export const AMP = new BN("100");
-export const LEVERAGE = AMP.mul(TWO); // leverage = amp * n_coins
-
-/**
- * @notice Send a transaction. Return result if successful, throw error if failed.
- */
+/// Send a transaction. Return result if successful, throw error if failed.
 export async function sendTransaction(
   terra: LocalTerra | LCDClient,
   sender: Wallet,
   msgs: Msg[],
   verbose = false
 ) {
-  const tx = await sender.createAndSignTx({
-    msgs,
-    fee: new StdFee(GAS_LIMIT, [
-      new Coin("uluna", GAS_AMOUNT),
-      new Coin("uusd", GAS_AMOUNT),
-    ]),
-  });
+  const GAS_LIMIT = 30000000;
+  const GAS_AMOUNT = 4500000;
+  const DEFAULT_FEE = new StdFee(GAS_LIMIT, [
+    new Coin("uluna", GAS_AMOUNT),
+    new Coin("uusd", GAS_AMOUNT),
+  ]);
 
+  const tx = await sender.createAndSignTx({ msgs, fee: DEFAULT_FEE });
   const result = await terra.tx.broadcast(tx);
 
   // Print the log info
@@ -75,14 +55,8 @@ export async function sendTransaction(
   return result;
 }
 
-/**
- * @notice Upload contract code to LocalTerra. Return code ID.
- */
-export async function storeCode(
-  terra: LocalTerra | LCDClient,
-  deployer: Wallet,
-  filepath: string
-) {
+/// Upload contract code to LocalTerra. Return code ID.
+export async function storeCode(terra: LCDClient, deployer: Wallet, filepath: string) {
   const code = fs.readFileSync(filepath).toString("base64");
   const result = await sendTransaction(terra, deployer, [
     new MsgStoreCode(deployer.key.accAddress, code),
@@ -90,20 +64,18 @@ export async function storeCode(
   return parseInt(result.logs[0].eventsByType.store_code.code_id[0]);
 }
 
-/**
- * @notice Instantiate a contract from an existing code ID. Return contract address.
- */
+/// Instantiate a contract from an existing code ID. Return contract address
 export async function instantiateContract(
-  terra: LocalTerra | LCDClient,
+  terra: LCDClient,
   deployer: Wallet,
-  admin: Wallet, // leave this emtpy then contract is not migratable
+  admin: Wallet | undefined, // leave this emtpy then contract is not migratable
   codeId: number,
   initMsg: object
 ) {
   const result = await sendTransaction(terra, deployer, [
     new MsgInstantiateContract(
       deployer.key.accAddress,
-      admin.key.accAddress,
+      admin ? admin.key.accAddress : undefined,
       codeId,
       initMsg
     ),
@@ -111,10 +83,20 @@ export async function instantiateContract(
   return result;
 }
 
-/**
- * @notice Return the native token balance of the specified account
- */
-export async function queryNativeTokenBalance(
+/// Find CW20 token balance of the specified account
+export async function queryCw20Balance(
+  terra: LCDClient,
+  account: string,
+  contract: string
+) {
+  const balanceResponse = await terra.wasm.contractQuery<{ balance: string }>(contract, {
+    balance: { address: account },
+  });
+  return balanceResponse.balance;
+}
+
+/// Find native token balance of the specified account
+export async function queryNativeBalance(
   terra: LocalTerra | LCDClient,
   account: string,
   denom: string = "uusd"
@@ -127,28 +109,16 @@ export async function queryNativeTokenBalance(
   }
 }
 
-/**
- * @notice Return CW20 token balance of the specified account
- */
-export async function queryTokenBalance(
-  terra: LocalTerra | LCDClient,
-  account: string,
-  contract: string
-) {
-  const balanceResponse = await terra.wasm.contractQuery<{ balance: string }>(contract, {
-    balance: { address: account },
-  });
-  return balanceResponse.balance;
+/// Encode a JSON object to base64 string
+export function toEncodedBinary(obj: any) {
+  return Buffer.from(JSON.stringify(obj)).toString("base64");
 }
 
-/**
- * @notice Given a total amount of UST, find the deviverable amount, after tax, if we
- * transfer this amount.
- * @param amount The total amount
- * @dev Assumes a tax rate of 0.001 and cap of 1000000 uusd.
- * @dev Assumes transferring UST. Transferring LUNA does not incur tax.
- */
+/// Given a total amount of UST, find the deviverable amount, after tax, if we transfer this amount
+/// NOTE: Assumes a tax rate of 0.1% and no tax cap (`must be configured in LocalTerra/config.genesis.json`)
 export function deductTax(amount: number) {
+  const DECIMAL_FRACTION = new BN("1000000000000000000");
+
   const tax = Math.min(
     amount -
       new BN(amount)
@@ -157,32 +127,19 @@ export function deductTax(amount: number) {
         .toNumber(),
     1000000
   );
+
   return amount - tax;
 }
 
-/**
- * @notice Given a intended deliverable amount, find the total amount, including tax,
- * necessary for deliver this amount. Opposite operation of `deductTax`.
- * @param amount The intended deliverable amount
- * @dev Assumes a tax rate of 0.001 and cap of 1000000 uusd.
- * @dev Assumes transferring UST. Transferring LUNA does not incur tax.
- */
+/// @notice Given a intended deliverable amount, find the total amount necessary for deliver this amount
+/// NOTE: Assumes a tax rate of 0.1% and no tax cap (`must be configured in LocalTerra/config.genesis.json`)
 export function addTax(amount: number) {
   const tax = Math.min(new BN(amount).div(new BN(1000)).toNumber(), 1000000);
   return amount + tax;
 }
 
-/**
- * @notice Encode a JSON object to base64 binary
- */
-export function toEncodedBinary(obj: any) {
-  return Buffer.from(JSON.stringify(obj)).toString("base64");
-}
-
-/**
- * @notice Calculate the output when swapping in a Uniswap V2-style pool
- */
-export function computeXykSwapOutput(
+/// @notice Calculate the output when swapping in a Uniswap V2-style pool
+export function computeSwapOutput(
   offerAmount: BN | number | string,
   offerDepth: BN | number | string,
   askDepth: BN | number | string
@@ -196,7 +153,9 @@ export function computeXykSwapOutput(
   const swapAmount = askDepth.sub(askDepthAfter);
 
   // commission rate = 0.3%
-  const commission = swapAmount.mul(THREE).div(THOUSAND);
+  const three = new BN("3");
+  const thousand = new BN("1000");
+  const commission = swapAmount.mul(three).div(thousand);
 
   // Note: return amount is after deducting commission but before duducting tax
   const returnAmount = swapAmount.sub(commission);
@@ -206,95 +165,4 @@ export function computeXykSwapOutput(
     returnAmount: returnAmount.toString(),
     commission: commission.toString(),
   };
-}
-
-/**
- * @notice Calculate the output when swapping in a Curve V1-style pool
- */
-export function computeStableSwapOutput(
-  offerAmount: BN | number | string,
-  offerDepth: BN | number | string,
-  askDepth: BN | number | string
-) {
-  offerAmount = new BN(offerAmount);
-  offerDepth = new BN(offerDepth);
-  askDepth = new BN(askDepth);
-
-  const d = _computeD(offerDepth, askDepth);
-  const askDepthAfter = _computeNewBalanceOut(offerDepth.add(offerAmount), d);
-  const swapAmount = askDepth.sub(askDepthAfter);
-
-  // commission rate = 0.3%
-  const commission = swapAmount.mul(THREE).div(THOUSAND);
-
-  // Note: return amount is after deducting commission but before duducting tax
-  const returnAmount = swapAmount.sub(commission);
-
-  return {
-    swapAmount: swapAmount.toString(),
-    returnAmount: returnAmount.toString(),
-    commission: commission.toString(),
-  };
-}
-
-/**
- * @notice Given asset amounts in a stable swap pool, calculate D iteratively
- */
-function _computeD(amountA: BN, amountB: BN) {
-  const amountATimesCoins = amountA.mul(TWO).add(ONE);
-  const amountBTimesCoins = amountB.mul(TWO).add(ONE);
-  const sumX = amountA.add(amountB);
-
-  let d = sumX;
-  let dPrev: BN;
-
-  for (let i = 0; i < ITERATIONS; i++) {
-    dPrev = d;
-
-    let dProd = d;
-    dProd = dProd.mul(d).div(amountATimesCoins);
-    dProd = dProd.mul(d).div(amountBTimesCoins);
-
-    // step
-    const leverageMul = LEVERAGE.mul(sumX);
-    const dProdMul = dProd.mul(TWO);
-    const lVal = leverageMul.add(dProdMul).mul(d);
-    const leverageSub = LEVERAGE.sub(ONE).mul(d);
-    const nCoinsSum = dProd.mul(TWO.add(ONE));
-    const rVal = leverageSub.add(nCoinsSum);
-
-    d = lVal.div(rVal);
-
-    if (d.eq(dPrev)) break;
-  }
-
-  return d;
-}
-
-/**
- * @notice Given D and final depth of offer asset (depth before the swap + offer amount),
- * calculate final depth of ask asset (depth before the swap - swap amount)
- */
-function _computeNewBalanceOut(newBalanceIn: BN, d: BN) {
-  const nom = d.pow(TWO.add(ONE));
-  const denom = new BN(newBalanceIn).mul(TWO.pow(TWO)).mul(LEVERAGE);
-  const c = nom.div(denom);
-
-  const b = new BN(newBalanceIn).add(d.div(LEVERAGE));
-
-  let y = d;
-  let yPrev: BN;
-
-  for (let i = 0; i < ITERATIONS; i++) {
-    yPrev = y;
-
-    const nom = y.pow(TWO).add(c);
-    const denom = y.mul(TWO).add(b).sub(d);
-
-    y = nom.div(denom);
-
-    if (y.eq(yPrev)) break;
-  }
-
-  return y;
 }
