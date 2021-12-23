7f5d2ce8ad2d5f87aa8ba474c0199bf4a8d5c722
The Larry <26318510+0xlarry@users.noreply.github.com>
2021-12-23 04:07:40 +0000

    Remove mock Anchor/Mirror; start working on mock Astro generator

--- contracts/mock-anchor/src/state.rs
+++
@@ -1,9 +0,0 @@
-use cosmwasm_std::{Addr, Uint128};
-use cw_storage_plus::{Item, Map};
-
-use crate::msg::ConfigBase;
-
-pub type Config = ConfigBase<Addr>;
-
-pub const CONFIG: Item<Config> = Item::new("config");
-pub const BOND_AMOUNT: Map<&Addr, Uint128> = Map::new("bond_amount");
--- contracts/mock-mirror/src/msg.rs
+++
@@ -1,9 +0,0 @@
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct ConfigBase<T> {
-    pub mirror_token: T,
-}
-
-pub type InstantiateMsg = ConfigBase<String>;
